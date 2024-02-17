c60b282691298ce010d5108f9e274f382030542b
Sezin EROL <fenciszn@hotmail.com>
2024-02-17 19:17:49 +0300

    Add project files.

---
+++ se/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! SE 100224");
+//110224
+//dotnet tool install --global NArchitecture.Gen --version 1.0.1
+//dotnet tool install --global NArchitecture.Gen --version 1.0.1
