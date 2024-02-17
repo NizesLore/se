4e305034fc701102dbb3b745110e30579aa93d07
Sezin EROL <fenciszn@hotmail.com>
2024-02-17 19:20:06 +0300

    se 170224

--- se/Program.cs
+++ se/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World! SE 100224");
 //110224
 //dotnet tool install --global NArchitecture.Gen --version 1.0.1
 //dotnet tool install --global NArchitecture.Gen --version 1.0.1
+//170224
